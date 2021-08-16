3918c11bc21a9de5e914a06df1fdd61bed2d7a3b
Zulfadli-Works <72394317+Zulfadli-Works@users.noreply.github.com>
2021-08-16 22:30:39 +0800

    .

--- Java/05 - Bicycle MVC/BicycleMVC/src/main/java/com/controller/Credentials.java
+++ Java/05 - Bicycle MVC/BicycleMVC/src/main/java/com/controller/Credentials.java
@@ -5,6 +5,6 @@ public class Credentials
 	//You will need an email with 2FA turned off
 	//on your email account, need to enable the allow less secure app access settings
 	//need to enter real gmail and password
-	static String email="fypboard@gmail.com";
-	static String pwd="G*^6VzCXh5";
+	static String email="";
+	static String pwd="";
 }
